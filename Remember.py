e35172838756c814e6f7a74fa8c75ce4f00ce4f9
Rishit Khare <AdarshKhare@Home-1s-MacBook-Pro.local>
2017-07-03 18:51:12 -0700

    Initial Code
    Basic bare bones of the code.

---
+++ Remember.py
@@ -0,0 +1,15 @@
+#Trying to create a program in which your name will be remembered.
+import time
+
+print("Hey there!")
+time.sleep(1)
+print("My name is...")
+time.sleep(1)
+print("...")
+time.sleep(2)
+print("...")
+time.sleep(3)
+print("Actually, I don't have a real name.")
+
+Name = str(input("What's yours?"))
+
